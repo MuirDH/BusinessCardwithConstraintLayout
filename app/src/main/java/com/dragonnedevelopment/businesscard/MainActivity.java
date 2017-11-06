135796767b8828fb49bff7f1c8a22eba36d1327b
Muir Halleron <muir.halleron@gmail.com>
2017-11-06 17:04:31 +0000

    Initial commit

---
+++ app/src/main/java/com/dragonnedevelopment/businesscard/MainActivity.java
@@ -0,0 +1,28 @@
+package com.dragonnedevelopment.businesscard;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        title = (TextView)findViewById(R.id.textView);
+
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, cardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dragonnedevelopment/businesscard/cardActivity.java
@@ -0,0 +1,17 @@
+package com.dragonnedevelopment.businesscard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * BusinessCard Created by Muir on 06/11/2017.
+ */
+
+public class cardActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card);
+    }
+}
